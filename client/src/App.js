25be9f7416b06b2137889fde959c43334c155f8c
nitikaborkar <nitikaborkar@gmail.com>
2024-02-24 17:04:24 +0800

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 // App.js
 import React from 'react';
 import EventsByLocation from './EventFinder'; // Import the FlightSearch component
+import HotelSearchForm from './hotel';
 
 
 function App() {
   return (
       <div className="App">
         <EventsByLocation /> {/* Use the FlightSearch component */}
+        <HotelSearchForm/>
       </div>
   );
 }
